1220caad76f1e71ecd3e12f189215cc46330995b
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-08-06 05:04:52 -0400

    Bigger oscillation

--- draw-scene.js
+++ draw-scene.js
@@ -39,14 +39,14 @@ drawDots = function() {
             let oy = y - 100;
             let dx = abs(x - 25);
             let dy = abs(y - 25);
-            let xx = x + cos(dx * 50 + t * 0.75) * map(dx + cos(x + y) * 4, 0, 50, 0, 20) * 0.5;
-            let yy = y + sin(dx * 50 + t * 0.75) * map(dy + sin(x) * 4, 0, 50, 0, 20) * 0.5;
+            let xx = x + cos(x * 50 + t * 0.75) * map(x + cos(x + y) * 4, 0, 50, 0, 20) * 0.5;
+            let yy = y + sin(x * 50 + t * 0.75) * map(y + sin(x) * 4, 0, 50, 0, 20) * 0.5;
 //             xx += cos(oy * 10) * sin(oy * 10);
 //             yy += cos(oy * 10) * sin(oy * 10);
             if (i == 0) {
                 oriX = xx;
             }
-            vertices.push((xx - 0) * 0.05 * 1.0 - 1.2, yy * 0.05 * 1.0 - 1.2, 0.0);
+            vertices.push((xx - 0) * 0.05 * 1.0 - 1.2, yy * 0.05 * 1.0 - 1., 0.0);
             i++;
         }
     }
