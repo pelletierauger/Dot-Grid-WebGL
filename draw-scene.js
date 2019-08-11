6b11b4757c72be3918ea0f529c095cba817009e2
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-08-11 03:32:18 -0400

    Horizontal, intricate coral 2

--- draw-scene.js
+++ draw-scene.js
@@ -44,10 +44,10 @@ drawDots = function() {
             let yy = y;
 //             xx += map(cos(x + t), -1, 1, 0.5, 0.4);
 //             yy += map(sin(y + t), -1, 1, 0.5, 0.4);
-            let m = 0.000125;
-            xx += (cos((i - 0) * 0.5 + ix * iy * m + t)) * 1.75;
-            xx += (sin(iy * 0.5 + ix * i * m + t)) * 1.75;
-            yy += (sin(ix * 0.5 + ix * i * m + t)) * 1.75;
+            let m = 0.00000125;
+            xx += (cos((i - 0) * 0.5 + i * i * m + t)) * 1.75;
+            xx += (sin(iy * 0.5 + i * i * m + t)) * 1.75;
+            yy += (sin(ix * 0.5 + i * i * m + t)) * 1.75;
 //             xx += cos((ix - 45) * (iy - 50) * 1.5e-2 + t);
 //             yy += sin(ix * (iy - 50) * 1.5e-2 + t);
             if (i == 0) {
