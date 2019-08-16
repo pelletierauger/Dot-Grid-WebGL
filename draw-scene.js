af4a5b089ad8a7910e46a5edee1603cb4877e4c4
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-08-16 15:59:51 -0400

    Differently torn

--- draw-scene.js
+++ draw-scene.js
@@ -53,8 +53,8 @@ drawDots = function() {
             let yy = y;
             let oX = oldArr[i].x;
             let oY = oldArr[i].y;
-            xx += cos(pow(oY, 0.7) * 0.9 + t * 2) * 4 + cos(oY * 0.5);
-            yy += sin(pow(oX, 0.7) * 0.9 + t * 2) * 4 + sin(oX * 0.5);
+            xx += cos(pow(oY, 0.7) * 0.9 + t * 2) * 5;
+            yy += sin(pow(oX, 0.7) * 0.9 + t * 2) * 5;
             newArr.push({x: xx, y: yy});
 //             
             let ranX = Math.random() * 0.025 * 0.5;
